23ef51ff99e85c7dcc2fe04ab43bc083d70f3416
= <=>
2020-03-10 21:41:59 +0100

    remove users reducer&actions - I'll use globalState hook instead

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,9 @@
 import { combineReducers, createStore } from 'redux';
+import taskManagerReducer from './taskManagerReducer';
 
 
 export const rootReducer = combineReducers({
-
+  taskManagerReducer,
 });
 
 export const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
