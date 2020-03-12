c783854a7b999859aff2fab4bd2b0c5808251bdd
= <=>
2020-03-12 21:55:56 +0100

    fix bad <Route/> prop name

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import WelcomePage from './Welcome/Welcome';
+import UserPage from './User/User';
 
 const Routes = () => {
   return (
     <Switch>
-      <Route to="/" exact component={WelcomePage}/>
-      <Route to="/user/:id" component={WelcomePage}/>
+      <Route path="/" exact component={WelcomePage}/>
+      <Route path="/user/:id" component={UserPage}/>
     </Switch>
   );
 };
