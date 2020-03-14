198dd68dbe001ac3fef689df7e4888641abd4e61
= <lukasz@lukaszrzasa.pl>
2020-03-14 19:08:44 +0100

    add icons to library

--- src/App.js
+++ src/App.js
@@ -7,11 +7,18 @@ import Routes from './pages/Routes';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme/theme';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faMapMarkerAlt,
+  faArrowLeft,
+  faPause,
+  faDoorOpen,
+  faCheck,
+  faDoorClosed, faCheckDouble
+} from '@fortawesome/free-solid-svg-icons';
 import {GlobalUserDataProvider} from './hooks/useUsersData';
 import GlobalStyle from './theme/GlobalStyle';
 
-library.add( faMapMarkerAlt );
+library.add( faMapMarkerAlt, faArrowLeft, faPause, faDoorOpen, faDoorClosed, faCheck, faCheckDouble );
 
 function App() {
   return (
