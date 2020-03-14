cacb6cbe569e2d3697580f57ef9ea82a9697e32c
= <lukasz@lukaszrzasa.pl>
2020-03-14 19:12:33 +0100

    fixes

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import WelcomePage from './Welcome/Welcome';
+import TaskManager from './TaskManager/TaskManager';
 import UserPage from './User/User';
 
 const Routes = () => {
   return (
     <Switch>
-      <Route path="/" exact component={WelcomePage}/>
+      <Route path="/" exact component={TaskManager}/>
       <Route path="/user/:id" component={UserPage}/>
     </Switch>
   );
