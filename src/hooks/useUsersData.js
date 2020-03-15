838de05e462b52c3738611abfcbcf01bcba9dee7
= <lukasz@lukaszrzasa.pl>
2020-03-15 19:04:44 +0100

    add helper functions

--- src/hooks/useUsersData.js
+++ src/hooks/useUsersData.js
@@ -10,6 +10,32 @@ export const GlobalUserDataProvider = ({children}) => {
   const [userData, setUserData] = useState(null);
   const [err, setErr] = useState(false);
 
+  const getIdsByName = filterBy => userData.filter(e=>{
+    // filter by first and last name
+    const name = e.first_name + ' ' + e.last_name;
+    return name.toLowerCase().indexOf(filterBy.toLowerCase())!==-1;
+    // TODO: filter by first&last name in reversed order
+  }).map(
+    e => e.id// separate id only
+  );
+
+  const getIdsByJobTitle = filterBy => userData.filter(
+    e => e.job_title.toLowerCase().indexOf(filterBy.toLowerCase())!==-1
+  ).map(
+    e => e.id
+  );
+
+  const getById = id => userData.find(e=>e.id===id);
+
+  const isProjectManager = id => {
+    if(id){
+      const user = getById(id);
+      if(user && user.job_title === 'Project Manager')
+        return true;
+    }
+    return false;
+  };
+
   const getData = async () => {
     try {
       const req = await axios.get(URL);
@@ -23,16 +49,22 @@ export const GlobalUserDataProvider = ({children}) => {
     getData();
   },[]);
 
-  return(<GlobalUserDataContext.Provider value={[userData]}>
+  return(<GlobalUserDataContext.Provider value={{userData, getIdsByName, getIdsByJobTitle, getById, isProjectManager}}>
     {userData !== null && children}
     {err && `Unable to download userData (${err})`}
   </GlobalUserDataContext.Provider>);
 };
 
 const useGlobalUserData = () => {
-  const [userData] = useContext(GlobalUserDataContext);
+  const {userData, getIdsByName, getIdsByJobTitle, getById, isProjectManager} = useContext(GlobalUserDataContext);
 
-  return userData;
+  return {
+    data: userData,
+    getIdsByName,
+    getIdsByJobTitle,
+    getById,
+    isProjectManager,
+  };
 };
 
 export default useGlobalUserData;
