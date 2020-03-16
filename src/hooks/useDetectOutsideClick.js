c6bc3965b91a93e3ab4da29265c6ddbcf3e8a823
= <lukasz@lukaszrzasa.pl>
2020-03-16 11:00:11 +0100

    delete Toggle provider && update logic && add useDetectOutsideClick hook instead

---
+++ src/hooks/useDetectOutsideClick.js
@@ -0,0 +1,18 @@
+import {useEffect} from 'react';
+
+const useDetectOutsideClick = (ref, handler) => {
+  const listener = ({target}) => {
+    if(!ref.current || ref.current.contains(target)){
+      return;
+    }
+    handler(false);
+  };
+  useEffect(()=>{
+    document.addEventListener('mousedown', listener);
+    return () => {
+      document.removeEventListener('mousedown', listener);
+    }
+  },[]);
+};
+
+export default useDetectOutsideClick;
