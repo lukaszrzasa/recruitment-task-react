574a4bc472a9a38298e62972fc765c2ea90664c6
= <lukasz@lukaszrzasa.pl>
2020-03-19 22:26:15 +0100

    fixes

--- src/components/atoms/helpers/getSize.js
+++ src/components/atoms/helpers/getSize.js
@@ -3,7 +3,6 @@ import theme from '../../../theme/theme';
 const { sizes } = theme;
 
 const getSize = (size) => {
-  console.log(size, sizes[size], Object.keys(sizes), Object.keys(sizes).indexOf(size));
   if(Object.keys(sizes).indexOf(size)===-1) return sizes.md;
   return sizes[size];
 };
