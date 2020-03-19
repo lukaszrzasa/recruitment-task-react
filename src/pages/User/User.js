4740a3e2599f37693f66bbe802a34de8f37ce0d7
= <lukasz@lukaszrzasa.pl>
2020-03-19 17:11:13 +0100

    add UserPage content

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -5,6 +5,8 @@ import UserPageTemplate from '../../components/templates/UserPage';
 import Button from '../../components/atoms/form/Button';
 import Icon from '../../components/atoms/text/Icon';
 import {Link} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
+import Error from '../../components/templates/Error';
 
 const Header = ()=><>
   <Button variant="gray" color="white" as={Link} to="/"><Icon icon="arrow-left"/></Button>
@@ -26,14 +28,30 @@ const MapContainer = ({position, zoom}) => {
 
 
 
-const UserPage = () => {
-  const userData = useGlobalUserData();
-  const position = [userData[2].lat, userData[5].lng];
+const UserPage = ({}) => {
+  const { getById } = useGlobalUserData();
+  const { id } = useParams();
+  const user = getById(id);
+  if(!user) return <Error
+    backTo="/"
+    errMsg="Nie znaleziono użutkownika."
+  />
+  const { first_name, last_name, lat, lng, avatar, job_title, description, email, street, city } = user;
+  const position = [lat, lng];
   const zoom = 13;
+
+  const contactInfo = [
+    { icon: 'at', text: email },
+    { icon: 'map-marker-alt', text: `${street}, ${city}` },
+  ];
+
   return (
     <UserPageTemplate
-      avatarSrc="https://via.placeholder.com/300x300"
-      userName="Pawian Wielki"
+      avatarSrc={avatar}
+      jobTitle={job_title}
+      description={description}
+      userName={`${first_name} ${last_name}`}
+      contactInfo={contactInfo}
       map={<MapContainer position={position} zoom={zoom} />}
       header={<Header/>}
     />
