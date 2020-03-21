2507c0670d460bd2aeed7f07cd4f9d1b5399b824
= <lukasz@lukaszrzasa.pl>
2020-03-21 01:13:32 +0100

    repair FormGroup styles

--- src/components/organisms/FormGroup.js
+++ src/components/organisms/FormGroup.js
@@ -1,37 +1,127 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import useDetectOutsideClick from '../../hooks/useDetectOutsideClick';
 import styled from 'styled-components';
+import Select from '../molecules/Select';
+import TextInput from '../atoms/form/TextInput';
+import Button from '../atoms/form/Button';
+import Icon from '../atoms/text/Icon';
 
-const Wrapper = styled.div`
-  
+
+const OutsideWrapper = styled.div`
+  display: flex;
+  align-items: ${({selectTop}) => selectTop ? 'flex-end' : 'flex-start'};
+  height: ${({theme}) => theme.sizes.xl}px;
+  width: ${({isVisible, theme}) => isVisible ? '480' : theme.sizes.xl}px;
+  transition: width .4s;
+  overflow: visible;
+  background-color: #fff;
+  border-radius: ${({theme}) => theme.sizes.xl/2}px;
+  position: relative;
+  padding-right: ${({theme}) => theme.sizes.xl-2}px;
+  border: solid 1px ${({theme}) => theme.colors.gray};
+  box-sizing: border-box;
 `;
 
-const FormFroup = ({select, input, button, isAlwaysVisible}) => {
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: ${({selectTop}) => selectTop ? 'flex-end' : 'flex-start'};
+  justify-content: flex-start;
+  position: relative;
+  overflow: hidden;
+  height: 140px;
+  width: 100%;
+`;
+
+const SelectWrapper = styled.div`
+  padding: 9px 5px 9px 20px;
+  width: 140px;
+  position: relative;
+  z-index: 1000;
+`;
+
+const InputWrapper = styled.div`
+  padding: 9px 15px 9px 5px;
+  flex: 1 auto;
+  position: relative;
+  z-index: 1000;
+  min-width: 100px;
+  input {
+    background-color: #eee;
+    padding: 0 5px;
+    border-radius: 4px;
+    width: 100%;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  z-index: 1000;
+  position: absolute;
+  top: -1px;
+  right: -1px;
+  outline: none;
+  height: height: ${({theme}) => theme.sizes.xl}px;;
+`;
+
+const FormGroup = ({options, mode, value, evSelect, setValue, selectTop}) => {
   const ref = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
+  const input = useRef(null);
   useDetectOutsideClick(ref, setIsVisible);
 
-  return (
-    <Wrapper ref={ref} isVisible={isVisible || isAlwaysVisible}>
-      <div>
-        {select}
-      </div>
-      <div>
-        {input}
-      </div>
-      <div>
-        {button}
-      </div>
+  const handleClick = () =>{
+    setIsVisible(!(isVisible || value.length));
+    setValue('');
+  };
+  useEffect(()=>{
+    if(isVisible && input.current)
+      setTimeout(()=>{
+        if(isVisible && input.current)
+          input.current.focus();
+      },400)
+  }, [isVisible]);
+
+  const visibility = () => isVisible || value.length;
+
+  return (<OutsideWrapper ref={ref} isVisible={visibility()} selectTop={selectTop}>
+    <Wrapper selectTop={selectTop}>
+      <SelectWrapper>
+        <Select
+          selectTop={selectTop}
+          mode={mode}
+          evSelect={evSelect}
+          options={options}
+        />
+      </SelectWrapper>
+      <InputWrapper>
+        <TextInput
+          ref={input}
+          value={value}
+          placeholder="Np."
+          onChange={({target}) => setValue(target.value)} />
+      </InputWrapper>
     </Wrapper>
-  );
+    <ButtonWrapper onClick={handleClick}>
+      <Button variant="gray" color="white">
+        <Icon icon={visibility() ? 'times' : 'search'} />
+      </Button>
+    </ButtonWrapper>
+  </OutsideWrapper>);
 };
 
-FormFroup.propTypes = {
-  select: PropTypes.node.isRequired,
-  input: PropTypes.node.isRequired,
-  button: PropTypes.node.isRequired,
-  isAlwaysVisible: PropTypes.bool,
+FormGroup.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
+  mode: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  evSelect: PropTypes.func.isRequired,
+  setValue: PropTypes.func.isRequired,
+  selectTop: PropTypes.bool,
 };
 
-export default FormFroup;
+export default FormGroup;
