07a86831ae8fade20b9958411c22debf2fd2e686
= <lukasz@lukaszrzasa.pl>
2020-03-21 08:22:20 +0100

    move UserAvatar to a shered component

--- src/pages/TaskManager/components/PageFooter.js
+++ src/pages/TaskManager/components/PageFooter.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import useGlobalUserData from '../../../hooks/useUsersData';
-import Avatar from '../../../components/atoms/interface/Avatar';
-import {Link} from 'react-router-dom';
-import Color from '../../../components/atoms/text/Color';
-import styled from 'styled-components';
 import Flex from '../../../components/atoms/interface/Flex';
 import {useDispatch, useSelector} from 'react-redux';
 import { setFilterUserMode, setFilterUserValue} from '../../../store/actions';
 import FormGroup from '../../../components/organisms/FormGroup';
+import UserAvatar from '../../../components/molecules/UserAvatar';
 
 const options = [
   {
@@ -19,23 +16,7 @@ const options = [
   }
 ];
 
-const Item = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  flex-direction: column;
-  padding: 0 5px;
-  width: 100px;
-  box-sizing: content-box;
-  span {
-    max-width: 100px;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    text-align: center;
-    font-size:72%;
-  }
-`;
+
 
 const PageFooter = () => {
   const { getAllByName } = useGlobalUserData();
@@ -62,15 +43,22 @@ const PageFooter = () => {
         evSelect={onSelect}
         setValue={onChange}
       />
-      {users.slice(0,10).map(({ id, first_name, last_name, avatar, job_title})=><Item as={Link} key={id} to={`/user/${id}`}>
-        <Avatar size="lg" src={avatar} />
-        <Color variant="default">
-          {first_name} {last_name}
-        </Color>
-        <Color variant="violet" isBlock>
-          <i>{job_title || '-'}</i>
-        </Color>
-      </Item>)}
+      {users.slice(0,10).map(
+        ({
+           id,
+           first_name,
+           last_name,
+           avatar,
+           job_title
+        }) => <UserAvatar
+          avatar={avatar}
+          firstName={first_name}
+          lastName={last_name}
+          jobTitle={job_title}
+          id={id}
+          key={id}
+        />)
+      }
       {users.length>10 && <Flex style={{alignSelf:'stretch',alignItems:'center'}}>
         <span>+ {users.length-10} więcej...</span>
       </Flex>}
