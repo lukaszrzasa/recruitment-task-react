08110c18a3917745eb3c36e31a9dc44c78ffa4ca
= <lukasz@lukaszrzasa.pl>
2020-03-22 17:13:04 +0100

    move icons from App --to--> registerIcon.js (required by some tests to register them)

--- src/App.js
+++ src/App.js
@@ -6,19 +6,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './pages/Routes';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme/theme';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faMapMarkerAlt, faArrowLeft, faPause,
-  faDoorOpen, faCheck, faDoorClosed,
-  faCheckDouble, faPlus, faTimes, faStar,
-  faPen, faAt, faSearch, faCaretDown,
-} from '@fortawesome/free-solid-svg-icons';
-import { faStar as faRegularStar } from '@fortawesome/free-regular-svg-icons';
 import {GlobalUserDataProvider} from './hooks/useUsersData';
 import GlobalStyle from './theme/GlobalStyle';
 import BodyModalProvider from './providers/BodyModal';
+import registerIcons from './registerIcons';
 
-library.add( faMapMarkerAlt, faArrowLeft, faPause, faDoorOpen, faDoorClosed, faCheck, faCheckDouble, faPlus, faTimes, faStar, faPen, faRegularStar, faAt, faSearch, faCaretDown );
+registerIcons();
 
 function App() {
   return (
