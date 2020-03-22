c908e0e71a023f37a1b7b43a498cc61f260ba045
= <lukasz@lukaszrzasa.pl>
2020-03-22 17:26:49 +0100

    add more validation

--- src/pages/TaskManager/helpers/move.js
+++ src/pages/TaskManager/helpers/move.js
@@ -1,12 +1,22 @@
 const move = (source, destination, droppableSource, droppableDestination) => {
 
+  const startIndex = droppableSource.index;
+  const endIndex   = droppableDestination.index;
+  if(startIndex > source.length
+    || startIndex < 0
+    || endIndex > destination.length
+    || endIndex < 0
+    || typeof startIndex !== 'number'
+    || typeof endIndex !== 'number'
+  ) return false;
+
   // clone original
   const sourceClone = [...source];
   const destClone = [...destination];
 
   // move item
-  const [removed] = sourceClone.splice(droppableSource.index, 1);
-  destClone.splice(droppableDestination.index, 0, removed);
+  const [removed] = sourceClone.splice(startIndex, 1);
+  destClone.splice(endIndex, 0, removed);
 
   // return object with mover arrays
   const result = {};
--- src/pages/TaskManager/helpers/reorder.js
+++ src/pages/TaskManager/helpers/reorder.js
@@ -7,17 +7,19 @@ const reorder = (list, droppableSource, droppableDestination) => {
     || startIndex < 0
     || endIndex > list.length
     || endIndex < 0
+    || typeof startIndex !== 'number'
+    || typeof endIndex !== 'number'
   ) return false;
 
   // clone original
   let destClone = [...list];
-  const result = {};
 
   // reorder
   const [removed] = destClone.splice(startIndex, 1);
   destClone.splice(endIndex, 0, removed);
 
   // return object with reordered array
+  const result = {};
   result[droppableDestination.droppableId] = destClone;
   return result;
 };
